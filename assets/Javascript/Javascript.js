c68b82871b04dd30bcf61924027afba7b4a384ba
Arielandriel30 <70600622+Arielandriel30@users.noreply.github.com>
2020-11-10 00:35:12 -0300

    Add files via upload

---
+++ assets/Javascript/Javascript.js
@@ -0,0 +1,14 @@
+  $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+
+   $('input').click(function(){
+    $('.cantidad').focus();
+        });
